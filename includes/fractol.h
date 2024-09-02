3f780830f96563ee9250b3d0ded225eb0268bd9b
jose5556 <josefilmeneses@gmail.com>
2024-09-02 19:02:49 +0100

    render a pixel

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 15:38:03 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/02 17:52:51 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/02 18:31:02 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,12 @@
 # include "../libs/libft/libft.h"
 # include <unistd.h>
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}  t_data;
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
 #endif
--- src/start.c
+++ src/start.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 13:39:18 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/02 17:53:36 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/02 18:47:57 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,22 @@ int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
-
-	ft_printf("%s", "ola");
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "hello world");
-	if (mlx_win == NULL)
-		return (1);
+	t_data	img;
+	
+	mlx = mlx_init();  //return a void * which holds the location of our current MLX instance
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "fractol");
+	img.img = mlx_new_image(mlx, 1920, 1080); //return a pointer to the window just created
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
+}
+
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
 }
