34cfc7862bf2fbcda800406fc08e2e66a0e937ca
jose5556 <josefilmeneses@gmail.com>
2024-09-02 17:53:49 +0100

    norminette fixed

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 15:38:03 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/02 17:51:03 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/02 17:52:51 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 
 # include "../libs/minilibx-linux/mlx.h"
 # include "../libs/libft/libft.h"
-#include <unistd.h>
+# include <unistd.h>
 
 #endif
