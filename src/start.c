731b1477dc7b78d7d6ce4dbd464fd6e2a0e67083
jose5556 <josefilmeneses@gmail.com>
2024-09-06 21:56:16 +0100

    destroy win and img

--- src/start.c
+++ src/start.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 13:39:18 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/06 19:54:18 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/06 21:53:02 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,38 @@
 
 int	main(void)
 {
-	mlx_vars	mlx_vars;
-	t_data		img;
+	t_data		data;
 
-	mlx_vars.mlx = mlx_init();//return a void * which holds the location of our current MLX instance
-	mlx_vars.win = mlx_new_window(mlx_vars.mlx, 1920, 1080, "fractol");
-	img.img = mlx_new_image(mlx_vars.mlx, 1920, 1080); //return a pointer to the window just created
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
-		&img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 5, 5, RED);
-	mlx_put_image_to_window(mlx_vars.mlx, mlx_vars.win, img.img, 0, 0);
-	mlx_hook(mlx_vars.win, ON_KEYDOWN, 1L<<0, close_program, &mlx_vars);
-	mlx_loop(mlx_vars.mlx);
+	data.vars.mlx = mlx_init(); //return a void * which holds the location of our current MLX instance
+	if (data.vars.mlx == NULL)
+		exit (0);
+	data.vars.win = mlx_new_window(data.vars.mlx, 1920, 1080, "fractol");
+	data.imgs.img = mlx_new_image(data.vars.mlx, 1920, 1080); //return a pointer to the window just created
+	data.imgs.addr = mlx_get_data_addr(data.imgs.img, &data.imgs.bits_per_pixel,
+		&data.imgs.line_length, &data.imgs.endian);
+	my_mlx_pixel_put(&data, 5, 5, RED);
+	mlx_put_image_to_window(data.vars.mlx, data.vars.win, data.imgs.img, 0, 0);
+	mlx_hook(data.vars.win, ON_KEYDOWN, 1L<<0, &close_program, &data.vars);
+	mlx_loop(data.vars.mlx);
 }
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->imgs.addr + (y * data->imgs.line_length + x * (data->imgs.bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
 
-int	close_program(int keycode, mlx_vars *vars)
+int	close_program(int keycode, t_data *data)
 {
 	if (keycode == XK_Escape)
 	{
-		mlx_destroy_window(vars->mlx, vars->win);
-		exit (1);
+		mlx_destroy_image(data->vars.mlx, data->imgs.img);
+		mlx_destroy_window(data->vars.mlx, data->vars.win);
+		mlx_destroy_display(data->vars.mlx);
+		free(data->vars.mlx);
+		exit (0);
 	}
 	return (1);
 }
