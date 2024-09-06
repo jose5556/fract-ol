8b8ef7a39a1fdba3b238ff0c73a003a5113b4cb1
jose5556 <josefilmeneses@gmail.com>
2024-09-06 23:54:53 +0100

    added handle_close

--- src/start.c
+++ src/start.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 13:39:18 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/06 22:34:53 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/06 22:51:51 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,19 @@ int	main(void)
 {
 	t_data		data;
 
-	data.vars.mlx = mlx_init(); //return a void * which holds the location of our current MLX instance
+	data.vars.mlx = mlx_init();
 	if (data.vars.mlx == NULL)
 		exit (0);
 	data.vars.win = mlx_new_window(data.vars.mlx, 1920, 1080, "fractol");
-	data.imgs.img = mlx_new_image(data.vars.mlx, 1920, 1080); //return a pointer to the window just created
+	data.imgs.img = mlx_new_image(data.vars.mlx, 1920, 1080);
 	data.imgs.addr = mlx_get_data_addr(data.imgs.img, &data.imgs.bits_per_pixel,
-		&data.imgs.line_length, &data.imgs.endian);
+			&data.imgs.line_length, &data.imgs.endian);
 	my_mlx_pixel_put(&data, 5, 5, RED);
-	mlx_put_image_to_window(data.vars.mlx, data.vars.win, data.imgs.img, 0, 0);
+	mlx_put_image_to_window(data.vars.mlx, data.vars.win,
+		data.imgs.img, 0, 0);
 	mlx_hook(data.vars.win, KeyPress, KeyPressMask, &close_program, &data.vars);
-	mlx_hook(data.vars.win, DestroyNotify, NoEventMask, &handle_close, &data.vars);
+	mlx_hook(data.vars.win, DestroyNotify, NoEventMask,
+		&handle_close, &data.vars);
 	mlx_loop(data.vars.mlx);
 }
 
@@ -34,16 +36,15 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->imgs.addr + (y * data->imgs.line_length + x * (data->imgs.bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	dst = data->imgs.addr + (y * data->imgs.line_length + x
+			* (data->imgs.bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 }
 
 int	close_program(int keycode, t_data *data)
 {
 	if (keycode == XK_Escape)
-	{
 		handle_close(data);
-	}
 	return (1);
 }
 
@@ -56,6 +57,3 @@ int	handle_close(t_data *data)
 	exit (0);
 	return (1);
 }
-
-
-
