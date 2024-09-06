94bbb769ba4be62f6d645817255ed0af3bc53320
jose5556 <josefilmeneses@gmail.com>
2024-09-06 22:40:57 +0100

    DestroyNotify handled

--- src/start.c
+++ src/start.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 13:39:18 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/06 22:16:01 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/06 22:34:53 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,13 @@ int	main(void)
 	if (data.vars.mlx == NULL)
 		exit (0);
 	data.vars.win = mlx_new_window(data.vars.mlx, 1920, 1080, "fractol");
-	//data.imgs.img = mlx_new_image(data.vars.mlx, 1920, 1080); //return a pointer to the window just created
-	//data.imgs.addr = mlx_get_data_addr(data.imgs.img, &data.imgs.bits_per_pixel,
-	//	&data.imgs.line_length, &data.imgs.endian);
-	//my_mlx_pixel_put(&data, 5, 5, RED);
-	//mlx_put_image_to_window(data.vars.mlx, data.vars.win, data.imgs.img, 0, 0);
-	//mlx_hook(data.vars.win, ON_KEYDOWN, 1L<<0, &close_program, &data.vars);
+	data.imgs.img = mlx_new_image(data.vars.mlx, 1920, 1080); //return a pointer to the window just created
+	data.imgs.addr = mlx_get_data_addr(data.imgs.img, &data.imgs.bits_per_pixel,
+		&data.imgs.line_length, &data.imgs.endian);
+	my_mlx_pixel_put(&data, 5, 5, RED);
+	mlx_put_image_to_window(data.vars.mlx, data.vars.win, data.imgs.img, 0, 0);
+	mlx_hook(data.vars.win, KeyPress, KeyPressMask, &close_program, &data.vars);
+	mlx_hook(data.vars.win, DestroyNotify, NoEventMask, &handle_close, &data.vars);
 	mlx_loop(data.vars.mlx);
 }
 
@@ -41,11 +42,20 @@ int	close_program(int keycode, t_data *data)
 {
 	if (keycode == XK_Escape)
 	{
-		mlx_destroy_image(data->vars.mlx, data->imgs.img);
-		mlx_destroy_window(data->vars.mlx, data->vars.win);
-		mlx_destroy_display(data->vars.mlx);
-		free(data->vars.mlx);
-		exit (0);
+		handle_close(data);
 	}
 	return (1);
 }
+
+int	handle_close(t_data *data)
+{
+	mlx_destroy_image(data->vars.mlx, data->imgs.img);
+	mlx_destroy_window(data->vars.mlx, data->vars.win);
+	mlx_destroy_display(data->vars.mlx);
+	free(data->vars.mlx);
+	exit (0);
+	return (1);
+}
+
+
+
