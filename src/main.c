4b98ddfd1194496011e1e8b19f8d6ff1d91f7281
jose5556 <josefilmeneses@gmail.com>
2024-09-20 08:20:33 +0100

    small bug fixed

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 06:41:55 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/20 07:01:48 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/20 08:13:31 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@ int main(int argc, char *argv[])
 {
 	t_data		data;
 
-	if (argc == 2 && !ft_strncmp(argv[2], "mandelbrot", 10) 
-			|| argc == 4 && !ft_strncmp(argv[2], "julia", 5))
+	if ((argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10))
+			|| (argc == 4 && !ft_strncmp(argv[1], "julia", 5)))
 	{
+		data.vars.name = argv[1];
 		fractal_init(&data);
-        fractal_render(&data);
+        //fractal_render(&data);
         mlx_loop(data.vars.mlx);		
 	}
 	else
 	{
 		ft_printf("%s", ERROR_MESSAGE);
-		exit(EXIT_FAILURE);
+		exit (EXIT_FAILURE);
 	}
 }
