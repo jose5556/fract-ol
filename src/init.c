5209dad695276a247e88c3503450664135466c7b
jose5556 <josefilmeneses@gmail.com>
2024-09-25 13:58:08 +0100

    malloc error exepcion remoced

--- src/init.c
+++ src/init.c
@@ -6,17 +6,11 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 07:10:11 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/25 11:34:35 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/25 13:29:32 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
-
-static void	malloc_error()
-{
-	ft_printf("%s", "error in some memory allocation, freeing all vars and aborting program");
-	exit (EXIT_FAILURE);
-}
 
 void	data_init(t_fractal *fractal)
 {
@@ -28,13 +22,13 @@ void    fractal_init(t_data *data)
 {
 	data->mlx = mlx_init();
 	if (data->mlx == NULL)
-		malloc_error();
+		exit (EXIT_FAILURE);
 	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, data->name);
 	if (data->win == NULL)
 	{
 		mlx_destroy_display(data->mlx);
 		free(data->mlx);
-		malloc_error();
+		exit (EXIT_FAILURE);
 	}
 	data->img.img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
 	if (data->img.img == NULL)
@@ -42,7 +36,7 @@ void    fractal_init(t_data *data)
 		mlx_destroy_window(data->mlx, data->win);
 		mlx_destroy_display(data->mlx);
 		free(data->mlx);
-		malloc_error();
+		exit (EXIT_FAILURE);
 	}
 	data->img.addr = mlx_get_data_addr(data->img.img, &data->img.bits_per_pixel,
 			&data->img.line_length, &data->img.endian);
