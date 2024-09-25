57ab566e9eeb8a66ea978b32536e8a3dbfd776a6
jose5556 <josefilmeneses@gmail.com>
2024-09-25 11:48:13 +0100

    created and populated data_init

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 07:10:11 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/21 08:05:38 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/25 11:34:35 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,33 @@ static void	malloc_error()
 	exit (EXIT_FAILURE);
 }
 
+void	data_init(t_fractal *fractal)
+{
+	fractal->hypotenuse = 4;
+	fractal->fractal_iterations = 100;
+}
+
 void    fractal_init(t_data *data)
 {
-	data->vars.mlx = mlx_init();
-	if (data->vars.mlx == NULL)
+	data->mlx = mlx_init();
+	if (data->mlx == NULL)
 		malloc_error();
-	data->vars.win = mlx_new_window(data->vars.mlx, HEIGHT, WIDTH, data->vars.name);
-	if (data->vars.win == NULL)
+	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, data->name);
+	if (data->win == NULL)
 	{
-		mlx_destroy_display(data->vars.mlx);
-		free(data->vars.mlx);
+		mlx_destroy_display(data->mlx);
+		free(data->mlx);
 		malloc_error();
 	}
-	data->imgs.img = mlx_new_image(data->vars.mlx, HEIGHT, WIDTH);
-	if (data->imgs.img == NULL)
+	data->img.img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
+	if (data->img.img == NULL)
 	{
-		mlx_destroy_window(data->vars.mlx, data->vars.win);
-		mlx_destroy_display(data->vars.mlx);
-		free(data->vars.mlx);
+		mlx_destroy_window(data->mlx, data->win);
+		mlx_destroy_display(data->mlx);
+		free(data->mlx);
 		malloc_error();
 	}
-	data->imgs.addr = mlx_get_data_addr(data->imgs.img, &data->imgs.bits_per_pixel,
-			&data->imgs.line_length, &data->imgs.endian);
+	data->img.addr = mlx_get_data_addr(data->img.img, &data->img.bits_per_pixel,
+			&data->img.line_length, &data->img.endian);
+	data_init(&data->fractal);
 }
