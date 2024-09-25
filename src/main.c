392a6652a4fb0127d749d9acfc7003de39ff3b36
jose5556 <josefilmeneses@gmail.com>
2024-09-25 11:48:44 +0100

    vars names changed

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 06:41:55 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/21 08:29:17 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/25 11:37:40 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int main(int argc, char *argv[])
 {
-	t_data		data;
+	t_data	data;
 
 	if ((argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10))
 			|| (argc == 4 && !ft_strncmp(argv[1], "julia", 5)))
 	{
-		data.vars.name = argv[1];
+		data.name = argv[1];
 		fractal_init(&data);
-        //fractal_render(&data);
-        mlx_loop(data.vars.mlx);		
+        render_fractal(&data);
+        mlx_loop(data.mlx);		
 	}
 	else
 	{
