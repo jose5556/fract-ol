3225bb28341722ba79a4f7a3aee3dd4aecc43e72
jose5556 <josefilmeneses@gmail.com>
2024-09-27 17:15:20 +0100

    smooth color done

--- src/render.c
+++ src/render.c
@@ -6,35 +6,48 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 07:47:35 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/27 13:00:33 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/27 17:12:23 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-void	handel_pixel(int x, int y, t_data *data)
+int smooth_color_interpolation(int color1, int color2, int i, int max_iterations)
 {
-	t_complex   a;
-    t_complex   b;
-	int			i;
-	int			color;
+    double  t;
+    
+    t = (double)i / max_iterations;
+    t = t * t * (3.0 - 2.0 * t);
+    int r = ((color1 >> 16) & 0xFF) + ((color2 >> 16) & 0xFF - (color1 >> 16) & 0xFF) * t;
+    int g = ((color1 >> 8) & 0xFF) + ((color2 >> 8) & 0xFF - (color1 >> 8) & 0xFF) * t;
+    int b = (color1 & 0xFF) + (color2 & 0xFF - color1 & 0xFF) * t;
+    return ((r << 16) | (g << 8) | b);
+}
 
-	i = -1;
-	a.x = 0;
+void handel_pixel(int x, int y, t_data *data)
+{
+    t_complex a;
+    t_complex b;
+    int i;
+    int color;
+
+    i = -1;
+    a.x = 0;
     a.y = 0;
-	b.x = linear_interpolation(-2, 2, x, WIDTH) + data->fractal.shift_x;
-	b.y = linear_interpolation(2, -2, y, HEIGHT) + data->fractal.shift_y;
-	while(++i < data->fractal.fractal_iterations)
-	{
-		a = sum_complex(square_complex(a), b);
-		if ((a.x * a.x) + (a.y * a.y) > data->fractal.hypotenuse)
-		{
-			color = linear_interpolation(BLACK, WHITE, i, data->fractal.fractal_iterations);
-			my_mlx_pixel_put(&data->img, x, y, color);
-			return ;
-		}
-	}
-	my_mlx_pixel_put(&data->img, x, y, PSYCHEDELIC_PURPLE);
+    b.x = (linear_interpolation(-2, 2, x, WIDTH) * data->fractal.zoom) + data->fractal.shift_x;
+    b.y = (linear_interpolation(2, -2, y, HEIGHT) * data->fractal.zoom) + data->fractal.shift_y;
+
+    while (++i < data->fractal.fractal_iterations)
+    {
+        a = sum_complex(square_complex(a), b);
+        if ((a.x * a.x) + (a.y * a.y) > data->fractal.hypotenuse)
+        {
+            color = smooth_color_interpolation(DARK_BLUE, WHITE, i, data->fractal.fractal_iterations);
+            my_mlx_pixel_put(&data->img, x, y, color);
+            return ;
+        }
+    }
+    my_mlx_pixel_put(&data->img, x, y, WHITE);
 }
 
 void	render_fractal(t_data *data)
@@ -43,10 +56,10 @@ void	render_fractal(t_data *data)
 	int	y;
 
 	y = -1;
-	while (++y <= HEIGHT)
+	while (++y < HEIGHT)
 	{
 		x = -1;
-		while (++x <= WIDTH)
+		while (++x < WIDTH)
 			handel_pixel(x, y, data);
 	}
 	mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
