573dc21ed7ac6f99072d25fbbd46c6a9b2316e67
jose5556 <josefilmeneses@gmail.com>
2024-09-27 13:06:17 +0100

    reorganize their names

--- src/hooks.c
+++ src/hooks.c
@@ -6,28 +6,41 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 11:54:18 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/25 13:37:08 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/09/27 13:04:59 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-void	handle_hooks(t_data *data)
+void	hooks_listener(t_data *data)
 {
 	mlx_hook(data->win, KeyPress, KeyPressMask, 
-		close_program, data);
+		handle_hooks, data);
 	mlx_hook(data->win, DestroyNotify, NoEventMask, 
-		handle_close, data);
+		close_program, data);
 }
-
-int	close_program(int keycode, t_data *data)
+ 
+int	handle_hooks(int keycode, t_data *data)
 {
 	if (keycode == XK_Escape)
-		handle_close(data);
-	return (1);
+		close_program(data);
+	if (keycode == XK_Left)
+		data->fractal.shift_x += 0.5;
+	if (keycode == XK_Right)
+		data->fractal.shift_x -= 0.5;
+	if (keycode == XK_Up)
+		data->fractal.shift_y -= 0.5;
+	if (keycode == XK_Down)
+		data->fractal.shift_y += 0.5;
+ 	if (keycode == XK_plus)
+		data->fractal.fractal_iterations += 10;
+	if (keycode == XK_minus)
+		data->fractal.fractal_iterations -= 10;
+	render_fractal(data);
+	return (0);
 }
 
-int	handle_close(t_data *data)
+int	close_program(t_data *data)
 {
 	mlx_destroy_image(data->mlx, data->img.img);
 	mlx_destroy_window(data->mlx, data->win);
