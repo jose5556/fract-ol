1a086468e657c9be22741cda5ba679b7b905ff22
jose5556 <josefilmeneses@gmail.com>
2024-10-03 18:11:37 +0100

    colors are nor hard coded now, norminette fixed

--- src/render.c
+++ src/render.c
@@ -6,55 +6,71 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 07:47:35 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/27 17:16:03 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/03 17:59:29 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-static int  smooth_color_interpolation(int color1, int color2, int i, int max_iterations)
+void	julia_mandelbrot(t_complex *a, t_complex *b, t_data *data)
 {
-    double  t;
-    
-    t = (double)i / max_iterations;
-    t = t * t * (3.0 - 2.0 * t);
-    int r = ((color1 >> 16) & 0xFF) + ((color2 >> 16) & 0xFF - (color1 >> 16) & 0xFF) * t;
-    int g = ((color1 >> 8) & 0xFF) + ((color2 >> 8) & 0xFF - (color1 >> 8) & 0xFF) * t;
-    int b = (color1 & 0xFF) + (color2 & 0xFF - color1 & 0xFF) * t;
-    return ((r << 16) | (g << 8) | b);
+	if (!ft_strncmp(data->name, "julia", 5))
+	{
+		b->x = data->fractal.julia_x;
+		b->y = data->fractal.julia_y;
+	}
+	else
+	{
+		b->x = a->x;
+		b->y = a->y;
+	}
 }
 
-void handel_pixel(int x, int y, t_data *data)
+int	handle_color(t_fractal *fractal, int color, int i)
 {
-    t_complex a;
-    t_complex b;
-    int i;
-    int color;
-
-    i = -1;
-    a.x = 0;
-    a.y = 0;
-    b.x = (linear_interpolation(-2, 2, x, WIDTH) * data->fractal.zoom) + data->fractal.shift_x;
-    b.y = (linear_interpolation(2, -2, y, HEIGHT) * data->fractal.zoom) + data->fractal.shift_y;
-
-    while (++i < data->fractal.fractal_iterations)
-    {
-        a = sum_complex(square_complex(a), b);
-        if ((a.x * a.x) + (a.y * a.y) > data->fractal.hypotenuse)
-        {
-            color = smooth_color_interpolation(DARK_BLUE, WHITE, i, data->fractal.fractal_iterations);
-            my_mlx_pixel_put(&data->img, x, y, color);
-            return ;
-        }
-    }
-    my_mlx_pixel_put(&data->img, x, y, WHITE);
+	if (fractal->color3)
+		color = smooth_color_interpolation(fractal->color1,
+				fractal->color2, i,
+				fractal->fractal_iterations);
+	else
+		color = linear_interpolation(fractal->color1,
+				fractal->color2, i,
+				fractal->fractal_iterations);
 }
 
-void	render_fractal(t_data *data)
+void	handel_pixel(int x, int y, t_data *data)
 {
-	int	x;
-	int	y;
+	t_complex	a;
+	t_complex	b;
+	int			i;
+	int			color;
 
+	i = 0;
+	a.x = (linear_interpolation(-2, 2, x, WIDTH) * data->fractal.zoom)
+		+ data->fractal.shift_x;
+	a.y = (linear_interpolation(2, -2, y, HEIGHT) * data->fractal.zoom)
+		+ data->fractal.shift_y;
+	julia_mandelbrot(&a, &b, data);
+	while (i++ < data->fractal.fractal_iterations)
+	{
+		a = sum_complex(square_complex(a), b);
+		if ((a.x * a.x) + (a.y * a.y) > data->fractal.hypotenuse)
+		{
+			color = handle_color(&data->fractal, color, i);
+			my_mlx_pixel_put(&data->img, x, y, color);
+			return ;
+		}
+	}
+	my_mlx_pixel_put(&data->img, x, y, data->fractal.lst.content);
+}
+
+int	render_fractal(void *param)
+{
+	int		x;
+	int		y;
+	t_data	*data;
+
+	data = (t_data *) param;
 	y = -1;
 	while (++y < HEIGHT)
 	{
@@ -63,9 +79,10 @@ void	render_fractal(t_data *data)
 			handel_pixel(x, y, data);
 	}
 	mlx_put_image_to_window(data->mlx, data->win, data->img.img, 0, 0);
+	return (1);
 }
 
-static void	my_mlx_pixel_put(t_img *vars, int x, int y, int color)
+void	my_mlx_pixel_put(t_img *vars, int x, int y, int color)
 {
 	char	*dst;
 
