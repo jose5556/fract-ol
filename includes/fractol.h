15e4bc5b5490b5363d36565cd7ff63695ab5d3ac
jose5556 <josefilmeneses@gmail.com>
2024-10-03 18:09:27 +0100

    added new colors, norminette fixed

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 15:38:03 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/09/27 17:16:43 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/03 18:06:23 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,14 @@
 # define HEIGHT	800
 # define WIDTH	800
 # define ERROR_MESSAGE "Wrong name or wrong format, please enter the name of your desired fractal first. If it is the julia set you want, don't forget to include their respective x and y coordinates.\n"
-# define BLACK       0x000000
-# define WHITE       0xFFFFFF
+# define BLACK		0x000000
+# define WHITE		0xFFFFFF
 # define RED		0x00FF0000
-# define LIGHT_BLUE 0xE0FFFF
+# define PURPLE		0x800080
 # define DARK_BLUE	0x00008B
+# define GREEN		0x006400
 
-
-
-typedef struct {
-    int r;
-    int g;
-    int b;
-} Color;
-
-typedef struct	s_img 
+typedef struct s_img
 {
 	void	*img;
 	char	*addr;
@@ -46,36 +39,51 @@ typedef struct	s_img
 	int		line_length;
 	int		endian;
 }	t_img;
-typedef struct	s_fractal
+typedef struct s_fractal
 {
 	double	hypotenuse;
 	int		fractal_iterations;
 	double	shift_x;
 	double	shift_y;
 	double	zoom;
+	double	julia_x;
+	double	julia_y;
+	int		color1;
+	int		color2;
+	int		color3;
+	t_list	lst;
 }	t_fractal;
-typedef struct	s_data
+typedef struct s_complex
+{
+	double	x;
+	double	y;
+}	t_complex;
+typedef struct s_data
 {
 	void		*mlx;
 	void		*win;
 	char		*name;
 	t_img		img;
 	t_fractal	fractal;
+	t_complex	complex;
 }	t_data;
-typedef struct s_complex 
-{
-	double	x;
-	double	y;
-}	t_complex;
-double		linear_interpolation(double minAllowed, double maxAllowed, double unscaledNum, double max);
-void    	fractal_init(t_data *data);
+double		linear_interpolation(double minAllowed,
+				double maxAllowed, double unscaledNum, double max);
+void		fractal_init(t_data *data);
 void		handel_pixel(int x, int y, t_data *data);
-void		render_fractal(t_data *data);
-t_complex   sum_complex(t_complex z1, t_complex z2);
-t_complex   square_complex(t_complex z);
+int			render_fractal(void *param);
+t_complex	sum_complex(t_complex z1, t_complex z2);
+t_complex	square_complex(t_complex z);
 int			hooks_listener(t_data *data);
 int			handle_keys(int keycode, t_data *data);
 int			close_program(t_data *data);
 int			handle_mouse(int button, int x, int y, t_data *data);
+void		my_mlx_pixel_put(t_img *vars, int x, int y, int color);
+double		ft_atoi_double(const char *nptr);
+void		julia_mandelbrot(t_complex *a, t_complex *b, t_data *data);
+int			handle_colors(int keycode, t_data *data);
+void		populate_color_lst(t_fractal *fractal);
+int			smooth_color_interpolation(int color1, int color2,
+				int i, int max_iterations);
 
 #endif
