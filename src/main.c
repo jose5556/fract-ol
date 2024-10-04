9e99120be41ee052a98957e4aad1c736c1e410dd
jose5556 <josefilmeneses@gmail.com>
2024-10-04 19:13:33 +0100

    much more simple now

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 06:41:55 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/03 17:30:54 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/04 19:06:31 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,16 @@ int	main(int argc, char *argv[])
 {
 	t_data	data;
 
-	if ((argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10))
-		|| (argc == 4 && !ft_strncmp(argv[1], "julia", 5)))
+	if (argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10))
+		mandelbrot_julia_handler(&data, argv[2]);
+	else if (argc == 4 && !ft_strncmp(argv[1], "julia", 5))
 	{
-		data.name = argv[1];
-		if (argc == 4 && !ft_strncmp(argv[1], "julia", 5))
-		{
-			data.fractal.julia_x = ft_atoi_double(argv[2]);
-			data.fractal.julia_y = ft_atoi_double(argv[3]);
-		}
-		fractal_init(&data);
-		render_fractal(&data);
-		hooks_listener(&data);
-		mlx_loop_hook(data.mlx, render_fractal, &data);
-		mlx_loop(data.mlx);
+		data.fractal.julia_x = ft_atoi_double(argv[2]);
+		data.fractal.julia_y = ft_atoi_double(argv[3]);
+		mandelbrot_julia_handler(&data, argv[4]);
 	}
+	else if (argc == 2 && !ft_strncmp(argv[1], "fern", 4))
+		fern_handler(&data);
 	else
 	{
 		ft_printf("%s", ERROR_MESSAGE);
