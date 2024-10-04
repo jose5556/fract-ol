cf4a314f33bc2c8721ee51582bdd68c8ae0e15e1
jose5556 <josefilmeneses@gmail.com>
2024-10-04 19:13:45 +0100

    random doubles gernerator

--- src/math_utils2.c
+++ src/math_utils2.c
@@ -6,15 +6,17 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 15:15:47 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/04 15:29:02 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:41:09 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-double lcg_random() {
-	unsigned long long seed = 6;
-	const unsigned long long a = 1664525;  // Multiplier
-	const unsigned long long c = 1013904223;  // Increment
-	const unsigned long long m = (1ULL << 32);  // 2^32 modulus
-    seed = (a * seed + c) % m;  // Update the seed
-    return (double)seed / (double)m;  // Normalize to [0, 1)
+#include "../includes/fractol.h"
+
+double	lcg_random(t_fern *fern) 
+{
+	const unsigned long long a = 16625;  // Multiplier
+	const unsigned long long c = 1013223;  // Increment
+	const unsigned long long m = 4297296;  // 2^32 modulus
+    fern->seed = (a * fern->seed + c) % m;
+    return (double)fern->seed / (double)m;
 }
