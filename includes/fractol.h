757dbad3306d42fb89d7249fcb00ec1c4f0ff8cc
jose5556 <josefilmeneses@gmail.com>
2024-10-08 06:09:17 +0100

    new colors and  new fern vars

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 15:38:03 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/04 19:07:23 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/08 06:05:31 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,22 @@
 						name of your desired fractal first. \
 						If it is the julia set you want, don't forget to \
 						include their respective x and y coordinates.\n"
-# define BLACK		0x000000
-# define WHITE		0xFFFFFF
-# define RED		0x00FF0000
-# define PURPLE		0x800080
-# define DARK_BLUE	0x00008B
-# define GREEN		0x006400
+# define BLACK				0x000000
+# define WHITE				0xFFFFFF
+# define RED				0xFF0000
+# define PURPLE				0x800080
+# define DARK_BLUE			0x00008B
+# define GREEN				0x00FF00
+# define GREEN_PLUS			0x007700
+# define MEDIUM_GREEN		0x00AA00
+# define DARK_GREEN			0x004400
+# define ORANGE				0xFFA500
+# define GOLDEN_YELLOW		0xFFD700
+# define BROWN				0x654321
+# define NEON_PINK			0xFF6EC7
+# define CYAN_BLUE			0x00E5FF
+# define ELECTRIC_VIOLET	0xB000FF
+# define LIME_GREEN			0x7FFF00
 
 typedef struct s_img
 {
@@ -45,8 +55,10 @@ typedef struct s_img
 }	t_img;
 typedef struct s_fern
 {
-	int				is_fern;
 	unsigned long	seed;
+	int				color;
+	int				swap_color;
+	int				first_iteration;
 }	t_fern;
 typedef struct s_fractal
 {
@@ -60,8 +72,8 @@ typedef struct s_fractal
 	int		color1;
 	int		color2;
 	int		color3;
-	t_fern	fern;
 	t_list	lst;
+	t_fern	fern;
 }	t_fractal;
 typedef struct s_complex
 {
@@ -96,8 +108,13 @@ void		populate_color_lst(t_fractal *fractal);
 int			smooth_color_interpolation(int color1, int color2,
 				int i, int max_iterations);
 double		lcg_random(t_fern *fern);
-void		render_fern(t_data *data);
+int			render_fern(t_data *data);
 void		mandelbrot_julia_handler(t_data *data, char *name);
-void		fern_handler(t_data *data);
+void		fern_handler(t_data *data, char *name);
+t_complex	handle_random(double random, t_complex a, t_complex b, t_data *data);
+void		chose_color(int i, t_fern *fern);
+int			hooks_listener_bonus(t_data *data);
+int			handle_keys_bonus(int keycode, t_data *data);
+void		delete(void *c);
 
 #endif
