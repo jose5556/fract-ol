a010ee7741cb67b0789d017cd2becbfb32c8fc1e
jose5556 <josefilmeneses@gmail.com>
2024-10-08 06:10:39 +0100

    organized

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 06:41:55 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/04 19:06:31 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/08 04:27:31 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ int	main(int argc, char *argv[])
 {
 	t_data	data;
 
+	ft_memset(&data, 0, sizeof(t_data));
 	if (argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10))
-		mandelbrot_julia_handler(&data, argv[2]);
+		mandelbrot_julia_handler(&data, argv[1]);
 	else if (argc == 4 && !ft_strncmp(argv[1], "julia", 5))
 	{
 		data.fractal.julia_x = ft_atoi_double(argv[2]);
 		data.fractal.julia_y = ft_atoi_double(argv[3]);
-		mandelbrot_julia_handler(&data, argv[4]);
+		mandelbrot_julia_handler(&data, argv[1]);
 	}
 	else if (argc == 2 && !ft_strncmp(argv[1], "fern", 4))
-		fern_handler(&data);
+	{
+		
+		fern_handler(&data, argv[1]);
+	}
 	else
 	{
 		ft_printf("%s", ERROR_MESSAGE);
