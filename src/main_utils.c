3d58f563507604821cccc86c950ad264d4c551cb
jose5556 <josefilmeneses@gmail.com>
2024-10-08 06:10:53 +0100

    hook listener added

--- src/main_utils.c
+++ src/main_utils.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:53:13 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/04 19:09:31 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/10/08 04:20:15 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,13 @@ void	mandelbrot_julia_handler(t_data *data, char *name)
 	mlx_loop(data->mlx);
 }
 
-void	fern_handler(t_data *data)
+void	fern_handler(t_data *data, char *name)
 {
-	data->fractal.fern.is_fern = 1;
+	data->name = name;
+	data->fractal.fern.swap_color = 1;
 	fractal_init(data);
 	render_fern(data);
-	//hooks_listener_bonus(&data); //TODO
+	hooks_listener_bonus(data);
+	mlx_loop_hook(data->mlx, render_fern, data);
 	mlx_loop(data->mlx);
 }
